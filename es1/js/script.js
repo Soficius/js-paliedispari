074097306b497e4810a89d01d2bf4455a6448895
Soficius <103771506+Soficius@users.noreply.github.com>
2022-06-29 16:37:46 +0200

    esercizio 1 finito

--- es1/js/script.js
+++ es1/js/script.js
@@ -1,14 +1,7 @@
-// !Domande da farsi quando scriviamo una funzione
-// ?1 - Come la chiamo?
-// ?2 - Ho bisogno di parametri? (se si quali?)
-// ?3 - Devo restituire qualcosa? (se si cosa? di che tipo?)
-// ?4 OPZIONALE:  Qualcosa  potrebbe andare storto? (se si, cosa?)
-
-
 // Chiedere all’utente di inserire una parola
 // Creare una funzione per capire se la parola inserita è palindroma
 
-// 1. devo chiedere all'utente si inserire una parola, quindi mi serve una costante e un prompt
+// 1. devo chiedere all'utente di inserire una parola, quindi mi serve una costante e un prompt
 // 2. devo dire alla mia funzione di controllare se la parola inserita è palindroma 
 // 3. per far ciò la parola inserita deve leggersi ugualmente anche al contrario es: anna
 // 4. restituisco il risultato (true or false) nel caso sia palindroma o meno
@@ -27,4 +20,23 @@
 //*     alert('la parola inserita non è palindroma')
 //* }
 
+// !Domande da farsi quando scriviamo una funzione
+// ?1 - Come la chiamo?
+// ?2 - Ho bisogno di parametri? (se si quali?)
+// ?3 - Devo restituire qualcosa? (se si cosa? di che tipo?)
+// ?4 OPZIONALE:  Qualcosa  potrebbe andare storto? (se si, cosa?)
+
 // ! stesso codice da eseguire con funzioni
+function isPalindromo(word){
+    let reverseWord = '' 
+    for(let i = word.length-1; i>= 0; i--){
+        reverseWord += word[i]
+    }
+    return word === reverseWord
+}
+if(isPalindromo(prompt('Inserisci una parola'))){
+    alert('la parola inserita è palindroma')
+} else{
+    alert('la parola inserita non è palindroma')
+}
+
